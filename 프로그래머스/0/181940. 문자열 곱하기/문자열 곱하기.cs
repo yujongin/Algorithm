9b05c9b46bccf1f88a88d60ea2d99bb5f51a5dc0
yujongin <48309991+yujongin@users.noreply.github.com>
2024-03-17 21:19:17 +0900

    [level 0] Title: 문자열 곱하기, Time: 0.59 ms, Memory: 31 MB -BaekjoonHub

--- 프로그래머스/0/181940. 문자열 곱하기/문자열 곱하기.cs
+++ 프로그래머스/0/181940. 문자열 곱하기/문자열 곱하기.cs
@@ -3,8 +3,8 @@ using System;
 public class Solution {
     public string solution(string my_string, int k) {
         string answer = "";
-        for(int i = 1; i<=k; i++){
-            answer = answer + my_string;    
+        for(int i = 0; i<k; i++){
+            answer += my_string;    
         }
         return answer;
     }
