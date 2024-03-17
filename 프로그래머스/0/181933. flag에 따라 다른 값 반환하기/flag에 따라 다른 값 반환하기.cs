aa08711fd844cd0c8d9ee8e6942dc59f24c0c3e8
yujongin <48309991+yujongin@users.noreply.github.com>
2024-03-17 21:11:01 +0900

    [level 0] Title: flag에 따라 다른 값 반환하기, Time: 0.24 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181933. flag에 따라 다른 값 반환하기/flag에 따라 다른 값 반환하기.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class Solution {
+    public int solution(int a, int b, bool flag) {
+        int answer = 0;
+        if(flag){
+            answer = a+b;
+        }else{
+            answer = a-b;
+        }
+        return answer;
+    }
+}
