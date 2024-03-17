dc2eb133f678c68173da6714c178d88983af5f3e
yujongin <48309991+yujongin@users.noreply.github.com>
2024-03-17 21:07:20 +0900

    [level 0] Title: 길이에 따른 연산, Time: 0.17 ms, Memory: 31.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181879. 길이에 따른 연산/길이에 따른 연산.cs
@@ -0,0 +1,19 @@
+using System;
+
+public class Solution {
+    public int solution(int[] num_list) {
+        int answer = 0;        
+        if(num_list.Length>=11){
+            for(int i = 0; i < num_list.Length; i++){
+                answer += num_list[i];
+            }
+        }else{
+            answer = 1;
+            for(int i = 0; i < num_list.Length; i++){
+                answer *= num_list[i];
+            }
+        }
+
+        return answer;
+    }
+}
