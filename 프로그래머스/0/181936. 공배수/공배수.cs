11d8ad0300e5b2e3d2fc910e5c380e422a434820
yujongin <48309991+yujongin@users.noreply.github.com>
2024-03-17 21:13:32 +0900

    [level 0] Title: 공배수, Time: 0.17 ms, Memory: 31.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181936. 공배수/공배수.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class Solution {
+    public int solution(int number, int n, int m) {
+        int answer = 0;
+        if(number % n == 0 && number % m == 0){
+            answer = 1;
+        }else{
+            answer = 0;
+        }
+        return answer;
+    }
+}
