133b574dc30900322c052da697642b68b797a2c3
yujongin <48309991+yujongin@users.noreply.github.com>
2024-03-17 22:06:49 +0900

    [level 0] Title: 문자열 붙여서 출력하기, Time: 16.90 ms, Memory: 16.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181946. 문자열 붙여서 출력하기/문자열 붙여서 출력하기.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class Example
+{
+    public static void Main()
+    {
+        String input = "";
+        Console.Clear();
+        input = Console.ReadLine().Replace(" ", "");
+        
+        Console.WriteLine(input);
+        
+        
+    }
+}
