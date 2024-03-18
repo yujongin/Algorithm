ca8a969810a0a4644a4820ea86e0092e6f9f883a
yujongin <48309991+yujongin@users.noreply.github.com>
2024-03-19 00:07:01 +0900

    [level 0] Title: 두 수의 연산값 비교하기, Time: 0.75 ms, Memory: 31.5 MB -BaekjoonHub

--- 프로그래머스/0/181938. 두 수의 연산값 비교하기/두 수의 연산값 비교하기.cs
+++ 프로그래머스/0/181938. 두 수의 연산값 비교하기/두 수의 연산값 비교하기.cs
@@ -4,7 +4,7 @@ public class Solution {
     public int solution(int a, int b) {
         int s = Int32.Parse(a.ToString()+b.ToString());
         int s2 = 2 * a * b;
-        int answer = s>=s2?s:s2;
+        int answer = Math.Max(s,s2);
         return answer;
     }
 }
