f4fd048961c138776b2ed168f29010ef548c4304
yujongin <48309991+yujongin@users.noreply.github.com>
2024-03-21 22:29:40 +0900

    [level 1] Title: 바탕화면 정리, Time: 0.41 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+public class Solution {
+    public int[] solution(string[] wallpaper) {
+        int minX = -1;
+        int maxX = -1;
+        int minY = -1;
+        int maxY = -1;
+        for(int i = 0; i < wallpaper.Length; i++){
+            for(int j =0; j<wallpaper[i].Length; j++){
+                if(wallpaper[i][j].ToString() == "#"){
+                    if(minX == -1)minX = i;
+                    if(maxX < i)maxX = i;
+                    if(minY==-1 || minY>j) minY = j;
+                    if(maxY < j)maxY = j;
+                }
+            }
+        }
+        
+        return new int[4]{minX,minY,maxX+1,maxY+1};
+    }
+}
