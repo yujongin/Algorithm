fc61ad9a62ecb647c0d6912c8b5215c5f17a2b37
yujongin <48309991+yujongin@users.noreply.github.com>
2024-03-21 21:42:47 +0900

    [level 0] Title: n의 배수, Time: 0.13 ms, Memory: 31.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181937. n의 배수/n의 배수.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class Solution {
+    public int solution(int num, int n) {
+        if(num % n == 0){
+            return 1;
+        }else{
+            return 0;
+        }
+        
+    }
+}
