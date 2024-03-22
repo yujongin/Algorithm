2338978a7308db33ac7ca6f7e9534b4cde7ce0f2
yujongin <48309991+yujongin@users.noreply.github.com>
2024-03-22 12:07:15 +0900

    [level 1] Title: 덧칠하기, Time: 0.27 ms, Memory: 32 MB -BaekjoonHub

--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.cs
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.cs
@@ -2,7 +2,6 @@ using System;
 using System.Linq;
 public class Solution {
     public int solution(int n, int m, int[] section) {
-        int x = section[0];//초기값
         int count = 0;
         int nextNum = 0;
         for(int i = 0; i<section.Length; i++){
