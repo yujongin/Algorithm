0978115b2c42abc430926bd5e88ba1850ed13039
yujongin <48309991+yujongin@users.noreply.github.com>
2024-03-25 22:47:44 +0900

    [level 0] Title: 코드 처리하기, Time: 0.40 ms, Memory: 31.3 MB -BaekjoonHub

--- 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.cs
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.cs
@@ -2,25 +2,18 @@ using System;
 
 public class Solution {
     public string solution(string code) {
-        int mode = 0;
+        bool mode = false;
         string ret = "";
         for(int idx = 0; idx<code.Length; idx++){
             if(code[idx].ToString()=="1"){
-                mode = mode==1?0:1;
+                mode = !mode;
             }else{
-                if(mode == 1){
-                    if(idx%2 ==1){
-                        ret = ret+code[idx].ToString();
-                    }
-                }else{
-                    if(idx%2 ==0){
-                        ret = ret+code[idx].ToString();
-                    }
+                if(mode&&idx%2 ==1){
+                    ret = ret+code[idx].ToString();
+                }else if(!mode&&idx%2 ==0){
+                    ret = ret+code[idx].ToString();
                 }
-
             }
-
-            
         }
         if(ret==""){
             ret = "EMPTY";
