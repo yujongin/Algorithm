5679ba5feb595471431a3b87244568c1fef0e7b5
yujongin <48309991+yujongin@users.noreply.github.com>
2024-09-08 19:16:46 +0900

    [Bronze V] Title: 고양이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.cc
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void){
+    cout << "\\    /\\" << "\n";
+    cout << " )  ( \')" << "\n";
+    cout << "(  /  )" << "\n";
+    cout << " \\(__)|" << "\n";
+}
