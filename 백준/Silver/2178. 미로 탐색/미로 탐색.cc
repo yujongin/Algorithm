32528b79d98e49888f5e317eb0c6df417bf445c9
Yu jong in <48309991+yujongin@users.noreply.github.com>
2025-03-24 18:13:10 +0900

    [Silver I] Title: 미로 탐색, Time: 0 ms, Memory: 2200 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.cc
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.cc
@@ -1,51 +1,46 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-#define X first
-#define Y second
-
-string board[101];
-int vis[101][101];
-
-int n,m;
+string board[102];
+int vis[102][102];
 
 int dx[4] = {1,0,-1,0};
 int dy[4] = {0,1,0,-1};
 
-int main(void)
-{
+int n,m;
+int main(void){
     ios::sync_with_stdio(0);
     cin.tie(0);
     
     cin >> n >> m;
     
-    for(int i = 0; i < n; i++)
-    {
+    for(int i = 0; i <n; i++)
         cin >> board[i];
-    }
     
-    for(int i = 0; i < n; i++)
-    {
-        fill(vis[i], vis[i]+m, -1);
-    }
+    for(int i=0; i<n; i++)
+        fill(vis[i], vis[i]+m,-1);
     
     queue<pair<int,int>> Q;
-    vis[0][0] = 0;
     Q.push({0,0});
+    vis[0][0] = 0;
     
-    while(!Q.empty())
-    {
-        auto cur = Q.front();Q.pop();
-        for(int i = 0; i < 4; i++)
-        {
-            int dirX = cur.X + dx[i];
-            int dirY = cur.Y + dy[i];
-            if(dirX < 0 || dirX >= n || dirY < 0 || dirY >= m) continue;
-            if(vis[dirX][dirY] >= 0 || board[dirX][dirY] != '1') continue;
+    while(!Q.empty()){
+        pair<int,int> cur;
+        cur = Q.front();
+        Q.pop();
+        for(int dir = 0; dir<4; dir++){
+            int nx = cur.first+dx[dir];
+            int ny = cur.second+dy[dir];
             
-            vis[dirX][dirY] = vis[cur.X][cur.Y] + 1;
-            Q.push({dirX,dirY});
+            if(nx<0||nx>=n||ny<0||ny>=m) continue;
+            if(board[nx][ny] != '1' || vis[nx][ny] >= 0) continue;
+            
+            vis[nx][ny] = vis[cur.first][cur.second]+1;
+            Q.push({nx,ny});
+            
         }
     }
+    
     cout << vis[n-1][m-1] + 1;
+
 }
